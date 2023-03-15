47030e6e6f5ba680654d78b13694f19fe1d7d5d8
JSlander <jhovanslander@hotmail.com>
2023-03-15 14:28:30 -0500

    lots of work done but not working

--- index.js
+++ index.js
@@ -1,49 +1,130 @@
-// TODO: Include packages needed for this application
-var fs = require('fs');
-var inquirer = require('inquirer')
-// TODO: Create an array of questions for user input
+const fs = require("fs");
+const inquirer = require("inquirer");
+const util = require('util');
+const api = require('./utils/api.js');
+const generateMarkdown = require("./utils/generateMarkdown");
+
 const questions = [
-    {
-        type: 'input',
-        message: "What is your project title?",
-        name: 'title',
-        validate: function (answer) {
-            if (answer.length < 1) {
-                return console.log("You must enter a title!");
-            }
-            return true;
-        }
+  {
+    type: "input",
+    message: "What is your project title?",
+    name: "title",
+    default: "Title",
+    validate: function (answer) {
+      if (answer.length < 1) {
+        return console.log("You must enter a title!");
+      }
+      return true;
     },
-    {
-        type: 'input',
-        message: "Give a brief description of your projet.",
-        name: 'description',
-        validate: function (answer) {
-            if (answer.length < 1) {
-                return console.log("Your description cannot be empty!");
-            }
-            return true;
-        }
+  },
+  {
+    type: "input",
+    message: "Give a brief description of your projet.",
+    name: "description",
+    default: "Description",
+    validate: function (answer) {
+      if (answer.length < 1) {
+        return console.log("Your description cannot be empty!");
+      }
+      return true;
     },
-    {
-        type: 'input',
-        message: "Add a table of contents",
-        name: 'table of contents',
+  },
+  {
+    type: "input",
+    message: "What is your GitHub username?",
+    name: "username",
+    default: "Username",
+    validate: function (answer) {
+      if (answer.length < 1) {
+        return console.log("Please enter a valid GitHub username.");
+      }
+      return true;
     },
+  },
+  {
+    type: "input",
+    message: "What is your GitHub repo called?",
+    name: "repo",
+    default: "Repo",
+    validate: function (answer) {
+      if (answer.length < 1) {
+        return console.log("Please enter a valid GitHub repo.");
+      }
+      return true;
+    },
+  },
+  {
+    type: "input",
+    message: "List the steps needed to install your project.",
+    name: "install",
+  },
+  {
+    type: "input",
+    message:
+      "Are there examples of or instructions for your project? List them here:",
+    name: "usage",
+  },
+  {
+    type: "input",
+    message:
+      "Can other developers can contribute to your project? Offer guidelines how here:",
+    name: "contributing",
+  },
+  {
+    type: "input",
+    message:
+      "List the tests written for your application here, and provide examples for how to use them.",
+    name: "tests",
+  },
+  {
+    type: "list",
+    message: "Which license will your project use?",
+    choices: [
+      "GNU AGPLv3",
+      "GNU GPLv3",
+      "GNU LGPLv3",
+      "Mozilla Public License 2.0",
+      "Apache License 2.0",
+      "MIT License",
+      "Boost Software License 1.0",
+      "The Unlicense",
+    ],
+    name: "license",
+  },
 ];
 
-// TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-    fs.writeFile(fileName, data, err => {
-        if (err) {
-            return console.log(err);
-        }
-        console.log("Success!")
-    });
+  fs.writeFile(fileName, data, (err) => {
+    if (err) {
+      return console.log(err);
+    }
+    console.log("Success!");
+  });
 }
-writeToFile()
-// TODO: Create a function to initialize app
-function init() {}
+writeToFile();
 
-// Function call to initialize app
+const writeFileAsync = util.promisify(writeToFile);
+
+async function init() {
+  try {
+    const userResponses = await inquirer.prompt(questions);
+    console.log("Your responses: ", userResponses);
+    console.log(
+      "Thank you for your responses! Fetching your GitHub data next..."
+    );
+
+    const userInfo = await api.getUser(userResponses);
+    console.log("Your GitHub user info: ", userInfo);
+
+    console.log("Generating your README next...");
+    const markdown = generateMarkdown(userResponses, userInfo);
+    console.log(markdown);
+
+    await writeFileAsync(fileName, markdown);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
 init();
+
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,176 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (!license) {
+    return "";
+  } else {
+    let badge;
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+    switch (license) {
+      case "GNU AGPLv3":
+        badge = { name: "GNU+AGPLv3", color: "orange" };
+        break;
+      case "GNU GPLv3":
+        badge = { name: "GNU+GPLv3", color: "red" };
+        break;
+      case "GNU LGPLv3":
+        badge = { name: "GNU+LGPLv3", color: "blue" };
+        break;
+      case "Mozilla Public License 2.0":
+        badge = { name: "Mozilla+2.0", color: "yellow" };
+        break;
+      case "Apache License 2.0":
+        badge = { name: "Apache+2.0", color: "green" };
+        break;
+      case "MIT License":
+        badge = { name: "MIT", color: "brightgreen" };
+        break;
+      case "Boost Software License 1.0":
+        badge = { name: "Boost+Software+1.0", color: "yellowgreen" };
+        break;
+      case "The Unlicense":
+        badge = { name: "The+Unlicense", color: "blueviolet" };
+        break;
+    }
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+    return `https://img.shields.io/static/v1?label=license&message=${badge.name}&color=${badge.color}`;
+  }
+}
 
-// TODO: Create a function to generate markdown for README
+function renderLicenseLink(license) {
+  if (!license) {
+    return "";
+  } else {
+    let path;
+
+    switch (license) {
+      case "GNU AGPLv3":
+        path = "agpl-3.0";
+        break;
+      case "GNU GPLv3":
+        path = "gpl-3.0";
+        break;
+      case "GNU LGPLv3":
+        path = "lgpl-3.0";
+        break;
+      case "Mozilla Public License 2.0":
+        path = "mpl-2.0";
+        break;
+      case "Apache License 2.0":
+        path = "apache-2.0";
+        break;
+      case "MIT License":
+        path = "mit";
+        break;
+      case "Boost Software License 1.0":
+        path = "bsl-1.0";
+        break;
+      case "The Unlicense":
+        path = "unlicense";
+        break;
+    }
+
+    return `https://choosealicense.com/licenses/${path}/`;
+  }
+}
+
+function renderLicenseSection(license) {
+  if (!license) {
+    return "";
+  } else {
+    const LicenseLink = renderLicenseLink(license);
+    const LicenseBadge = renderLicenseBadge(license);
+    return `# This project is licensed under ${license}, found at ${LicenseLink}
+    ${LicenseBadge}`;
+  }
+}
+
 function generateMarkdown(data) {
-  return `# ${data.title}
+  const LicenseSection = renderLicenseSection(data.license);
+  let tableOfCont = `## Table of Contents`;
 
-`;
+  if (data.installation !== "") {
+    tableOfCont += `
+    * [Installation](#installation)`;
+  }
+
+  if (data.usage !== "") {
+    tableOfCont += `
+    * [Usage](#usage)`;
+  }
+
+  if (data.contributing !== "") {
+    tableOfCont += `
+    * [Contributing](#contributing)`;
+  }
+
+  if (data.tests !== "") {
+    tableOfCont += `
+    * [Tests](#tests)`;
+  }
+  const Markdown = `# ${data.title}
+    
+    ## Description 
+    
+    ${data.description}
+    `;
+
+
+  Markdown += tableOfCont;
+
+
+  Markdown += `
+    * [License](#license)`;
+
+  if (data.install !== "") {
+    Markdown += `
+    
+    ## Installation:
+    
+    ${data.install}`;
+  }
+
+  if (data.usage !== "") {
+    Markdown += `
+    
+    ## Usage:
+    
+    ${data.usage}`;
+  }
+
+  if (data.contributing !== "") {
+    Markdown += `
+    
+    ## Contributing:
+    
+    ${data.contributing}`;
+  }
+
+  if (data.tests !== "") {
+    Markdown += `
+    
+    ## Tests:
+    
+    ${data.tests}`;
+  }
+
+  Markdown += `
+    
+    ## License:
+    
+    ${data.license}
+    ${LicenseSection}
+    `;
+
+  const developer = `
+    ## Creator:
+
+    *${data.username}*
+
+    *${data.repo}*
+    `;
+
+  Markdown += developer;
+
+  return Markdown;
 }
 
 module.exports = generateMarkdown;
